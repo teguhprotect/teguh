99696d0b14a4cb80e872af504f1c53a6f9f802d1
Teguh <66298846+teguhprotect@users.noreply.github.com>
2020-06-16 08:22:39 +0700

    Update copet4.py

--- copet4.py
+++ copet4.py
@@ -37,23 +37,11 @@ except ImportError:
     import urllib2
 #=====================================================================
 #=====================================================================
-try:
-    header = "ios_ipad"
-    auth = "Z6vMBEnkp04n"
-    result = json.loads(requests.get("https://api.boteater.us/line_qr_v2?header="+header+"&auth="+auth).text)
-    print("Login IP: {}".format(result["result"]["login_ip"]))
-    print("QR Link: {}".format(result["result"]["qr_link"]))
-    result = json.loads(requests.get(result["result"]["callback"]+"&auth="+auth).text)
-    if result["status"] != 200:
-      raise Exception("Timeout!!!")
-    print("Pincode: "+result["result"]["pin_code"])
-    result = json.loads(requests.get(result["result"]["callback"]+"&auth="+auth).text)
-    if result["status"] != 200:
-      raise Exception("Timeout!!!")
-    ais = LINE(result["result"]["token"],appName="IOSIPAD\t9.18.1\tiPhone X\t12.4.1")
-    print("Login Sukses")
-except:pass
-#=====================================================================
+cl = LINE("zarkoni929@gmail.com","polytron11")
+cl.log("Auth Token : " + str(cl.authToken))
+cl.log("Timeline Token : " + str(cl.tl.channelAccessToken))
+print ("╔══════════════════\n╠ ʟᴏɢɪɴ sᴜᴄᴄᴇs\n╚══════════════════")
+#======================================================
 #Bots = [ais]
 #Team = owner + admin + staff 
 #ais = LINE("ud8546c432f683c3aceaf645594910d3f:aWF0OiAxNTg4NzU5NzI3MDk4Cg==..nQpkWRyZvJh+gb1YS9+kRgwHjhc=",appName="IOS\t10.1.1\tIOS\t13.3.1")
